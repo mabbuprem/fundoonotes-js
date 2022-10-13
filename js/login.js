88c97b66d1b6f77f5adc2f74bf47884bfa5736cb
mabbuprem <mabbupremkumar@gmail.com>
2022-10-13 21:55:44 +0530

    [prem]:registation validation completed

---
+++ js/login.js
@@ -0,0 +1,53 @@
+window.addEventListener("DOMContentLoaded",function(){
+    console.log("hi");
+    let emailServer,passwordServer;
+    let email1= document.querySelector(".form_input");
+
+    let password1=document.querySelector(".form_input1");
+    let button = document.querySelector(".next")
+    let emailHelper = document.querySelector(".helperTextEmail")
+    let passwordHelper = document.querySelector(".helperTextPassword")
+
+    let regexEmail = RegExp('^[a-zA-Z]+[a-zA-Z0-9]*[- . + _]?[a-zA-Z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]+[.]?[a-z]+$');
+    let regexPass = RegExp('^[A-Z]{1}[a-z]{1,}[0-9]{1,}$');
+
+    email1.addEventListener("keyup",function(){
+        emailServer=email1.value
+    })
+
+    password1.addEventListener("keyup",function(){
+        passwordServer=password1.value
+    })
+
+    button.addEventListener("click",function(e){
+        e.preventDefault()
+
+        let emailStatus = regexEmail.test(emailServer);
+        let passwordStatus = regexPass.test(passwordServer);
+
+
+        //Email Validation
+        if(emailStatus === true){
+            email1.style.border="1px solid #dddddd"
+            emailHelper.innerHTML=""
+        }
+        else if(emailStatus === false){
+            email1.style.border="1px solid red"
+            emailHelper.innerHTML="Please enter a valid email address."
+            emailHelper.style.color="red"
+        }
+
+        //Password Validation
+        if(passwordStatus === true){
+            password1.style.border="1px solid #dddddd"
+            passwordHelper.innerHTML=""
+        }
+        else if(passwordStatus === false){
+            password1.style.border="1px solid red"
+            passwordHelper.innerHTML="Please enter correct password."
+            passwordHelper.style.color="red"
+        }
+})
+}
+
+)
--- js/registration.js
+++ js/registration.js
@@ -1,81 +1,117 @@
-window.addEventListener('DOMContentLoaded',function(){
+window.addEventListener("DOMContentLoaded", function () {
+
+    console.log("this is my first java script");
+
+    let fnameServer, lnameServer, passwordMainServer, passwordConfirmServer, usernameServer;
+
+    let fname = document.querySelector("#firstname");
+    let lname = document.querySelector("#lastname");
+    let passwordM = document.querySelector("#passwordMain1");
+    let passwordC = document.querySelector("#passwordConfirm1");
+    let userName = document.querySelector("#username");
+    let button = document.querySelector(".next")
+
+    let fnameHelper = document.querySelector(".helperTextFName")
+    let lnameHelper = document.querySelector(".helperTextLName")
+    let userNameHelper = document.querySelector(".helperTextUsername")
+    let passwordMHelper = document.querySelector(".helperTextMPassword")
+    let passwordCHelper = document.querySelector(".helperTextCPassword")
+
+    let regexfName = RegExp('^[A-Z]{1}[a-z]{3,}$');
+    let regexlName = RegExp('^[A-Z]{1}[a-z]{3,}$');
+    let regexuserName = RegExp('^[a-zA-Z]+[a-zA-Z0-9]*[- . + _]?[a-zA-Z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]+[.]?[a-z]+$');
+    let regexMainPassword = RegExp('^[A-Z]{1}[a-z]{1,3}[0-9]{1,5}$');
+    let regexConfirmPassword = RegExp('^[A-Z]{1}[a-z]{1,3}[0-9]{1,5}$');
+
+    fname.addEventListener("keyup", function () {
+        fnameServer = fname.value;
+    })
+
+    lname.addEventListener("keyup", function () {
+        lnameServer = lname.value;
+    })
+
+    passwordM.addEventListener("keyup", function () {
+        passwordMainServer = passwordM.value;
+    })
+
+    passwordC.addEventListener("keyup", function () {
+        passwordConfirmServer = passwordC.value;
+    })
 
 
-    let regexName =RegExp('^[A-Z]{1}[a-z]{2,}$');
-    let regexLName =RegExp('^[A-Z]{1}[a-z]{0,}$');
-    let regexEmail =RegExp('^[a-z]{2,}[@][a-z]{2,5}[.][a-z]{3}$');
-    let regexPass =RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0,9])(?=.*[@!#])$');
-
-    let fName=document.getElementById('firstName')
-    let lName=document.getElementById('lastName')
-    let uName=document.getElementById('userName')
-    let pswd=document.getElementById('password')
-    let cpswd=document.getElementById('cpassword')
-    let nxt=document.getElementById('btn');
-
-    let a;
-    let fn=0,ln=0,em=0,psw=0,cpsw=0,nt=0;
+    userName.addEventListener("keyup", function () {
+        usernameServer = userName.value;
+    })
 
 
-    const showError = (input,message,oldcls,newcls,divid) =>{
-        document.getElementById(divid).classList.remove(oldcls);
-        document.getElementById(divid).classList.add(oldcls);
-        document.getElementById(input).innerHTML= message;
-        document.getElementById(input).style.color="black";
-        return false;
-    };
-
-    const showSuccess = (input,oldcls,newcls,divid) =>{
-        document.getElementById(input).textContent = "";
-        document.getElementById(divid).classList.add(oldcls);
-        document.getElementById(divid).classList.remove(newcls);
-        return true;
+    button.addEventListener("click", function (e) {
+        e.preventDefault()
 
 
+        let fnameStatus = regexfName.test(fnameServer);
+        let lnameStatus = regexlName.test(lnameServer);
+        let usernameStatus = regexuserName.test(usernameServer);
+        let pConfirmStatus = regexMainPassword.test(passwordConfirmServer);
+        let pMainStatus = regexConfirmPassword.test(passwordMainServer);
 
-};
+        //First Name Validation
+        if (fnameStatus === true) {
+            fname.style.border = "1px solid #dddddd"
+            fnameHelper.innerHTML = ""
+        }
+        else if (fnameStatus === false) {
+            fname.style.border = "1px solid red"
+            fnameHelper.innerHTML = "Please enter a valid first name."
+            fnameHelper.style.color = "red"
+        }
 
-fName.addEventListener('keyup', ()=>{
-    fn=check(fName,'name','first',regexName)
-    console.log("first name",fn);
-});
 
-lName.addEventListener('keyup', ()=>{
-    fn=check(fName,'name','last',regexLName)
-    console.log("last name",ln);
-});
+        //Last Name Validation
+        if (lnameStatus === true) {
+            lname.style.border = "1px solid #dddddd"
+            lnameHelper.innerHTML = ""
+        }
+        else if (lnameStatus === false) {
+            lname.style.border = "1px solid red"
+            lnameHelper.innerHTML = "Please enter a valid last name."
+            lnameHelper.style.color = "red"
+        }
 
-uName.addEventListener('keyup', ()=>{
-    fn=check(fName,'username','user',regexEmail)
-    console.log("user name",em);
-});
 
-pswd.addEventListener('keyup', ()=>{
-    fn=check(fName,'pass','pas', regexPass)
-    console.log("password",psw);
-});
+        //Password Validation
+        if (usernameStatus === true) {
+            userName.style.border = "1px solid #dddddd"
+            userNameHelper.innerHTML = ""
+        }
+        else if (usernameStatus === false) {
+            userName.style.border = "1px solid red"
+            userNameHelper.innerHTML = "Please enter valid user name."
+            userNameHelper.style.color = "red"
+        }
 
-cpswd.addEventListener('keyup', ()=>{
-    fn=check(fName,'pass','psd', regexPass)
-    console.log("cpassword",cpsw);
-});
+        //Password Main Validation
+        if (pMainStatus === true) {
+            passwordM.style.border = "1px solid #dddddd"
+            passwordMHelper.innerHTML = ""
+        }
+        else if (pMainStatus === false) {
+            passwordM.style.border = "1px solid red"
+            passwordMHelper.innerHTML = "Please enter a valid last name."
+            passwordMHelper.style.color = "red"
+        }
 
-nxt.addEventListener('keyup', ()=>{
-    fn=check(fName,'nxt', regexPass)
-    console.log(nt);
-});
-
-function check(inputid,errid,oldcls,newcls,reg){
-    if (!reg.text(inputid,value)){
-        a = showError(errid,"use valid value",oldcls,newcls,divid);
-        console.log("show error value",a);
-        return 0;
-    } else{
-        a = showSuccess(errid,oldcls,newcls,divid);
-        console.log("show success value",a);
-        return 1;
-    }
-};
+        //Password COnfirm Validation
+        if (pConfirmStatus === true) {
+            passwordC.style.border = "1px solid #dddddd"
+            passwordCHelper.innerHTML = ""
+        }
+        else if (pConfirmStatus === false) {
+            passwordC.style.border = "1px solid red"
+            passwordCHelper.innerHTML = "Please enter a valid last name."
+            passwordCHelper.style.color = "red"
+        }
+    })
 }
 
 )
