abcb0f2b5a57f47b72d71f99267ee5260f2a79f0
mabbuprem <mabbupremkumar@gmail.com>
2022-10-17 09:47:40 +0530

    [prem] ADDED: working on create note design part

---
+++ js/dashboard.js
@@ -0,0 +1,37 @@
+
+ let flag = true;
+ function openDrawer() {
+    console.log('when click drop down open')
+     if (flag) {
+         flag = false;
+         $(".drawer").css("width", "200px");
+         $(".myDrawer").css("width", "200px");
+         $(".drawer").css("border-radius", "0px 25px 25px 0px");
+         $(".drawer-span").css("display", "flex");
+         $(".drawer-span").css("margin-left", "30px");
+         $(".main-section").css("left", "45%");
+         $(".item-list").css("left", "15%");
+         $(".Div1").css("left", "15%");
+         $("#main").css("left", "15%");
+     }
+     else {
+         flag = true;
+         $(".drawer").css("width", "48px");
+         $(".drawer").css("border-radius", "50%");
+         $(".myDrawer").css("width", "65px");
+         $(".drawer-span").css("display", "none");
+         $(".main-section").css("left", "22%");
+         $(".item-list").css("left", "10%");
+     }
+ }
+ 
+ var toggle = document.getElementById("toggle");
+ var content = document.getElementById("content");
+ 
+//  toggle.addEventListener("click", function() {
+//      content.style.display = (content.dataset.toggled ^= 1) ? "block" : "none";
+//  });
+ function openNote(){
+     document.querySelector("#toggle").style.display = "block";
+     document.querySelector("#user-note").style.display = "block";
+ }
