12a22cf525bc9ae5d94545fd635c4bf7d1744e9b
mabbuprem <mabbupremkumar@gmail.com>
2022-10-25 08:13:18 +0530

    [prem] ADDED:working and delete note and color note

--- js/Db.js
+++ js/Db.js
@@ -1,22 +1,26 @@
 window.addEventListener('DOMContentLoaded', function () {
 
-    let descriptionServer, titleServer;
+    let descriptionServer, titleServer, archiveStatusServer;
     let title2 = document.querySelector(".titleMain");
     let titleDescription = document.querySelector(".takeNote")
     let descIcon = document.querySelector(".descriptionIcon")
     let bottomIcon = document.querySelector(".mainBottomIcons")
     let closeB = document.querySelector(".closeButton")
     let modeltitle = document.querySelector("#takeNoteTitleInput")
-    let archiveButton = document.querySelector(".ArchiveB")
+    let archiveButton = document.querySelector(".prem")
     console.log(modeltitle)
     let modelDescription = document.querySelector("#takeNoteDescrptionInput")
     console.log(modelDescription);
     let token = localStorage.getItem('token');
+    // let mybtn = document.getElementById("myBtn")
     getallnotes();
+    // ArchiveNotes()
+
 
 
     titleServer = "";
     descriptionServer = ""
+    archiveStatusServer = false
 
 
     title2.addEventListener("change", function () {
@@ -27,6 +31,21 @@ window.addEventListener('DOMContentLoaded', function () {
     titleDescription.addEventListener("change", function () {
         descriptionServer = titleDescription.value
     })
+
+    archiveButton.addEventListener("click", function (event) {
+        event.preventDefault()
+        archiveStatusServer = true
+        console.log(archiveStatusServer)
+    })
+
+
+    console.log(archiveStatusServer)
+
+
+    if (archiveStatusServer === true) {
+
+        console.log("Hey its true from if condition to check if archive button is clicked")
+    }
 
 
     $(document).ready(function () {
@@ -91,7 +110,7 @@ window.addEventListener('DOMContentLoaded', function () {
 
 
     })
-
+    //get all notes
     function getallnotes() {
         console.log("get all notes")
         $.ajax({
@@ -121,65 +140,104 @@ window.addEventListener('DOMContentLoaded', function () {
                 <img
                     class="icons"  src="../assets/person_add_FILL0_wght400_GRAD0_opsz48 (1).svg" alt="">
                 </div>
-                <div>
-                <img 
+                <div class="btn-group dropup" id="color-palette-dropdown">
+        <button class="btn-btn-primary prem" type="button" id="btn-colors" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown"  aria-expanded="false">
+        <img 
                class="icons" src="../assets/palette_FILL0_wght400_GRAD0_opsz48 (1).svg" alt="">
-                </div>
+        </button>
+        <div class="color-palette dropdown-menu" id ="color-palette">
+            <div class="bg-white circled"></div>
+            <div class="bg-red"></div>
+            <div class="bg-orange"></div>
+            <div class="bg-yellow"></div>
+            <div class="bg-green"></div>
+            <div class="bg-turquoise"></div>
+            <div class="bg-blue"></div>
+            <div class="bg-dark-blue"></div>
+            <div class="bg-purple"></div>
+            <div class="bg-pink"></div>
+            <div class="bg-brown"></div>
+            <div class="bg-grey"></div>
+        </div>
+    </div>
                 <div>
                 <img 
                class="icons" src="../assets/image_FILL0_wght400_GRAD0_opsz48 (1).svg" alt="">
                 </div>
                 <div id="ArchiveB">
-                <button class="btn-btn-primary prem"><img 
-                    class="icons"    src="../assets/archive_FILL0_wght400_GRAD0_opsz48 (1).svg" alt=""></button>
+                <button @onclick="ArchiveNotes()" class="btn-btn-primary prem"><img  
+                    class="icons"   src="../assets/archive_FILL0_wght400_GRAD0_opsz48 (1).svg" alt=""></button>
                 </div>
-                <div>
-                <img 
-                    class="icons"  src="../assets/more_vert_FILL0_wght400_GRAD0_opsz48 (1).svg" alt="">
+                <div class="dropdown">
+                <button class="btn-btn-primary prem" id="myBtn" class="dropbtn"><img 
+                class="icons"  src="../assets/more_vert_FILL0_wght400_GRAD0_opsz48 (1).svg" alt=""></button>
+                <div id="myDropdown" class="dropdown-content">
+                <a href=>Delete Note</a>
+                <a href=>Add label</a>
+                <a href=>Add drawing</a>
+                <a href=>make a copy</a>
+                <a href=>show checkbox</a>
+                  
                 </div>
+              </div>
                
                 </div>
             </div>
-                
+
+
                 
                 `)
-
-
-
-            }
-
-        })
-    }
-    archiveButton.addEventListener("click", function (event) {
-        event.preventDefault()
-        let data ={
-            notearray : [event.currentTarget.id],
-            isArchived : true,
-           
-    }
-       
-        console.log("show data")
-
-        $.ajax({
-            url: "http://127.0.0.1:8000/api/archivenote",
-            type: "POST",
-            data: JSON.stringify(data),
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + token,
             },
-            success: function (result) {
-                console.log(result);
 
 
+            ArchiveNotes() {
+                let data = {
+                    notearray: [currentTarget.id],
+                    isArchived: true,
+
+                }
+                console.log("show data")
+
+                // $(document).ready(function () {
+                $.ajax({
+                    url: "http://127.0.0.1:8000/api/archivenote",
+                    type: "POST",
+                    data: JSON.stringify(data),
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': 'Bearer ' + token,
+                    },
+                    success: function (result) {
+                        console.log(result);
+
+
+                    }
+
+                })
             }
+
 
         })
 
 
+   // Get the button, and when the user clicks on it, execute myFunction
+   document.getElementById("myBtn").onclick = function() {myFunction()};
 
-    })
+   /* myFunction toggles between adding and removing the show class, which is used to hide and show the dropdown content */
+   function myFunction() {
+   document.getElementById("myDropdown").classList.toggle("show");
+}
+
+
+
+    }
     
+
+
+
+
+
+
 
 })
 
